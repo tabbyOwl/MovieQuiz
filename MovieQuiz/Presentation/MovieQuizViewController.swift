8c14a18a78555781211815e8a18f5335434cf39f
Svetlana <spo_17@mail.ru>
2025-10-26 13:35:58 +0800

    Add initial storyboard layout

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -4,6 +4,10 @@ final class MovieQuizViewController: UIViewController {
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+      
+        
+       
+        
     }
 }
 
